f4cbfeb4b8230a88199035776ae74582e3983dda
Brianhin94 <brianhin64@gmail.com>
2021-09-30 04:28:50 -0400

    home could use more style but im happy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,43 @@
 import React from "react";
-import image from "../portb.jpg";
+import image from "../profile.jpg";
 
 export default function Home() {
   return (
     <main>
-      <section className="relative flex bg-green-500 justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
-          Hello home.
-        </h1>
-        <div></div>
+      <section>
+
+        <div class="bg-green-600 w-full min-h-screen">
+          <div class=" max-w-6xl mx-auto px-4 py-6 flex justify-between md:flex-no-wrap flex-wrap">
+            <div class="md:w-1/3 w-full">
+           <img src={image} alt="Profile" class=" mx-auto max-width: 28rem;"></img>
+      
+    
+        
+
+              <section class="mt-14">
+                <div class="h-1 bg-green w-48 my-4">
+                </div>
+                <p class="text-black">I am a motivated and aspiring web developer with branding and trends knowledge. My goal is to learn more knowledge of the software industry and use my conversation skills to help Clients by exceeding their expectations.</p>
+              </section>
+
+              <section class="mt-  justify-end mx-auto ">
+                <h3 class="uppercase text-black font-medium text-3xl">Specializations</h3>
+                <div class="h-1 bg-green w-48 my-4">
+                </div>
+                <ul class="text-black list-disc list-inside">
+                  <li>Front End Design (HTML, CSS, Javascript)</li>
+                  <li><a href="https://tailwindcss.com" class="hover:underline">TailwindCSS (♥)</a></li>
+                  <li>Front End Development (Vue.js, React.js, Svelte)</li>
+                  <li>Design Thinking & Problem Solving </li>
+                  <li>Can develop front-end interfaces which interacts with backend API</li>
+                </ul>
+              </section>
+            </div>
+
+
+          </div>
+        </div>
+
       </section>
     </main>
   );
